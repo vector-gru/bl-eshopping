87caa347553f90401ab71876dfa5d550250b4002
vector-gru <popelouis1@outlook.com>
2024-08-25 18:02:47 +0100

    added wishlist

--- cart.php
+++ cart.php
@@ -10,6 +10,10 @@
     include ('Template/_cart-template.php');
     /* !include cart-template section */
 
+    /* include wishlist-template section */
+    include ('Template/_wishlist_template.php');
+    /* !include wishlist-template section */
+
 
     /* include new-arrivals section */
     include ('Template/_new-arrivals.php');
